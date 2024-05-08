21609d9ef9b89e60c6a3f58dd04a685285c3a791
jose <jose@thoughtmachine.net>
2024-05-08 17:05:57 +0100

    started on autocompletion setup

--- after/plugin/please.lua
+++ after/plugin/please.lua
@@ -6,4 +6,6 @@ vim.keymap.set('n', '<leader>pb', please.build)
 -- map please test to <space>pt
 vim.keymap.set('n', '<leader>pt', please.test)
 
-
+vim.keymap.set('n', '<leader>ptt', function()
+  require('please').test({ under_cursor = true })
+end)
